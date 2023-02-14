980e487f84e380f0eae825fe163551b00531063d
Nick Santana <nick@mobilecoin.com>
2023-02-14 08:53:05 -0800

    added security.
    Working on tests to verify the short circuit evaluation

--- verification/traits/src/lib.rs
+++ verification/traits/src/lib.rs
@@ -3,6 +3,7 @@
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
+use std::any::Any;
 use std::fmt::{Debug, Formatter};
 
 type Result<T> = std::result::Result<T, VerificationError>;
@@ -19,6 +20,7 @@ impl<S: Into<String>> From<S> for VerificationError {
 
 trait VerificationStep {
     fn verify(&self) -> Result<()>;
+    fn as_any(&self) -> &dyn Any;
 }
 
 impl Debug for dyn VerificationStep {
@@ -38,6 +40,9 @@ impl VerificationStep for And {
         self.left.verify()?;
         self.right.verify()
     }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 #[derive(Debug)]
@@ -50,6 +55,9 @@ impl VerificationStep for Or {
     fn verify(&self) -> Result<()> {
         self.left.verify().or(self.right.verify())
     }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -58,6 +66,9 @@ pub struct AlwaysTrue;
 impl VerificationStep for AlwaysTrue {
     fn verify(&self) -> Result<()> {
         Ok(())
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -68,24 +79,43 @@ impl VerificationStep for AlwaysFalse {
     fn verify(&self) -> Result<()> {
         Err(VerificationError::from("AlwaysFalse"))
     }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::cell::Cell;
     use super::*;
 
     pub struct Node {
         pub succeed: bool,
         pub message: String,
+        pub verified_called: Cell<bool>,
+    }
+
+    impl Node {
+        pub fn new(succeed: bool, message: impl Into<String>) -> Self {
+            Self {
+                succeed,
+                message: message.into(),
+                verified_called: Cell::new(false),
+            }
+        }
     }
 
     impl VerificationStep for Node {
         fn verify(&self) -> Result<()> {
+            self.verified_called.replace(true);
             if self.succeed {
                 Ok(())
             } else {
                 Err(VerificationError::from(self.message.clone()))
             }
+        }
+        fn as_any(&self) -> &dyn Any {
+            self
         }
     }
 
@@ -101,19 +131,41 @@ mod tests {
     #[test]
     fn and_short_circuits() {
         let and = And {
-            left: Box::new(Node{ succeed: false, message: "First".to_string()}),
-            right: Box::new(Node{ succeed: true, message: "Second".to_string()}),
+            left: Box::new(Node::new(false, "First")),
+            right: Box::new(Node::new(true, "Second")),
         };
         assert_eq!(and.verify(), Err(VerificationError::from("First")));
+        let right = and.right.as_any().downcast_ref::<Node>().expect("Should be a Node");
+        assert_eq!(right.verified_called.get(), false);
     }
 
     #[test]
     fn and_fails_on_tail() {
         let and = And {
-            left: Box::new(Node{ succeed: true, message: "First".to_string()}),
-            right: Box::new(Node{ succeed: false, message: "Second".to_string()}),
+            left: Box::new(Node::new(false, "First")),
+            right: Box::new(Node::new(true, "Second")),
         };
+        let left = and.left.as_any().downcast_ref::<Node>().expect("Should be a Node");
+        assert_eq!(left.verified_called.get(), true);
         assert_eq!(and.verify(), Err(VerificationError::from("Second")));
+    }
+
+    #[test]
+    fn or_fails_for_both_failing() {
+        let or = Or {
+            left: Box::new(AlwaysFalse),
+            right: Box::new(AlwaysFalse),
+        };
+        assert_eq!(or.verify(), Err(VerificationError::from("AlwaysFalse")));
+    }
+
+    #[test]
+    fn or_short_circuits() {
+        let or = Or {
+            left: Box::new(AlwaysTrue),
+            right: Box::new(AlwaysFalse),
+        };
+        assert_eq!(or.verify(), Ok(()));
     }
 
     #[test]
